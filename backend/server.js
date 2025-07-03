3469894bc7d052f07f343d096c46fcfbcfb7bd8f
alex <alex.escobar@epn.edu.ec>
2025-07-02 19:15:00 -0500

    Renombrar la carpeta frontend por la carpeta public, y moverlos dentro de la carpeta backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const feedbackRoutes = require('./routes/feedback.js');
-
+const path = require('path');
 const app = express();
+app.use(express.json());
 
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, 'public')));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB conectado'))
